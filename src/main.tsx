df1ab37329d630cadea046466a099360d57c64b1
Joaquin Osorio Arjona <joaosoarj@hotmail.com>
2024-08-08 13:01:43 +0200

    Actualizando
    Actualizando

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ defineMapElements(window, {
   resourcesUrl: "https://js.arcgis.com/map-components/4.30/assets",
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
